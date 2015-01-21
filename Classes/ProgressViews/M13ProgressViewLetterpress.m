3ced22282896a10d9ae422b3da3ea982c4b55fa8
Mark Turner <mt@papercloud.com.au>
2015-01-21 18:03:44 +1100

    CADisplayLinks are removed by invalidating rather than removing straight from run loop. Resolves Marxon13/M13ProgressSuite#54

--- Classes/ProgressViews/M13ProgressViewLetterpress.m
+++ Classes/ProgressViews/M13ProgressViewLetterpress.m
@@ -170,7 +170,7 @@
     if (animated == NO) {
         if (_displayLink) {
             //Kill running animations
-            [_displayLink removeFromRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
+            [_displayLink invalidate];
             _displayLink = nil;
         }
         [super setProgress:progress animated:NO];
@@ -181,7 +181,7 @@
         _animationToValue = progress;
         if (!_displayLink) {
             //Create and setup the display link
-            [self.displayLink removeFromRunLoop:NSRunLoop.mainRunLoop forMode:NSRunLoopCommonModes];
+            [self.displayLink invalidate];
             self.displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(animateProgress:)];
             [self.displayLink addToRunLoop:NSRunLoop.mainRunLoop forMode:NSRunLoopCommonModes];
         } /*else {
@@ -196,7 +196,7 @@
         CGFloat dt = (displayLink.timestamp - _animationStartTime) / self.animationDuration;
         if (dt >= 1.0) {
             //Order is important! Otherwise concurrency will cause errors, because setProgress: will detect an animation in progress and try to stop it by itself. Once over one, set to actual progress amount. Animation is over.
-            [self.displayLink removeFromRunLoop:NSRunLoop.mainRunLoop forMode:NSRunLoopCommonModes];
+            [self.displayLink invalidate];
             self.displayLink = nil;
             [super setProgress:_animationToValue animated:NO];
             [self setNeedsDisplay];
