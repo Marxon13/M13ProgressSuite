3e57c3d528c94b171ba82c1dd7957dbc64120903
Rusty Zarse <rusty@levo.us>
2015-08-10 16:22:47 -0400

    added framework support

---
+++ M13ProgressSuiteFramework/M13ProgressSuiteFramework.h
@@ -0,0 +1,19 @@
+//
+//  M13ProgressSuiteFramework.h
+//  M13ProgressSuiteFramework
+//
+//  Created by Rusty Zarse on 8/10/15.
+//  Copyright (c) 2015 Brandon McQuilkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for M13ProgressSuiteFramework.
+FOUNDATION_EXPORT double M13ProgressSuiteFrameworkVersionNumber;
+
+//! Project version string for M13ProgressSuiteFramework.
+FOUNDATION_EXPORT const unsigned char M13ProgressSuiteFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <M13ProgressSuiteFramework/PublicHeader.h>
+
+
