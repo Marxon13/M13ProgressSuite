5fa55e41256d382507bc867287aaa937b04d9ba8
BlueVirusX <patrick.ganaus@outlook.com>
2016-06-13 15:01:21 +0200

    Fixed framework product name Fixed module warnings

---
+++ M13ProgressSuiteFramework/M13ProgressSuite.h
@@ -0,0 +1,35 @@
+//
+//  M13ProgressSuiteFramework.h
+//  M13ProgressSuiteFramework
+//
+//  Created by Rusty Zarse on 8/10/15.
+//  Copyright (c) 2015 Brandon McQuilkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for M13ProgressSuiteFramework.
+FOUNDATION_EXPORT double M13ProgressSuiteFrameworkVersionNumber;
+
+//! Project version string for M13ProgressSuiteFramework.
+FOUNDATION_EXPORT const unsigned char M13ProgressSuiteFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <M13ProgressSuiteFramework/PublicHeader.h>
+#import <M13ProgressSuite/M13ProgressConsole.h>
+#import <M13ProgressSuite/M13ProgressHUD.h>
+#import <M13ProgressSuite/UIImage+ImageEffects.h>
+#import <M13ProgressSuite/UINavigationController+M13ProgressViewBar.h>
+#import <M13ProgressSuite/M13ProgressView.h>
+#import <M13ProgressSuite/M13ProgressViewBar.h>
+#import <M13ProgressSuite/M13ProgressViewBorderedBar.h>
+#import <M13ProgressSuite/M13ProgressViewFilteredImage.h>
+#import <M13ProgressSuite/M13ProgressViewImage.h>
+#import <M13ProgressSuite/M13ProgressViewLetterpress.h>
+#import <M13ProgressSuite/M13ProgressViewMetro.h>
+#import <M13ProgressSuite/M13ProgressViewMetroDotPolygon.h>
+#import <M13ProgressSuite/M13ProgressViewPie.h>
+#import <M13ProgressSuite/M13ProgressViewRadiative.h>
+#import <M13ProgressSuite/M13ProgressViewRing.h>
+#import <M13ProgressSuite/M13ProgressViewSegmentedBar.h>
+#import <M13ProgressSuite/M13ProgressViewSegmentedRing.h>
+#import <M13ProgressSuite/M13ProgressViewStripedBar.h>
