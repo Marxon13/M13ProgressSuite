6565aade5aa749c9bb7a6a48d1aa716a1af88037
Rafael Fernández <rafael.fernandez@sdos.es>
2020-10-14 12:37:09 +0200

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "M13ProgressSuite",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "M13ProgressSuite",
+            targets: ["M13ProgressSuite"])
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "M13ProgressSuite",
+            path: "Classes",
+            publicHeadersPath: "",
+            cSettings: [
+                .headerSearchPath("Application"),
+                .headerSearchPath("Console"),
+                .headerSearchPath("HUD"),
+                .headerSearchPath("NavigationController"),
+                .headerSearchPath("ProgressViews"),
+                  ]
+        ),
+    ]
+)
